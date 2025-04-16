bef9441278cfbd40085d94f21144a841d69d6b87
SonnySalva <sonnysalvatierra@yahoo.com>
2025-04-16 15:45:57 +1200

    add express and app.js

---
+++ jsfunction.js
@@ -0,0 +1,13 @@
+let multiple = function multiplication(p1, p2) {
+    console.log("p1 = ", p1);
+    console.log("p2 = ", p2);
+    return p1 * p2;
+  }
+
+  let multiple1 = (p1, p2) => p1 * p2;
+
+  console.log("Functions")
+
+  console.log("multiple >>", multiple)
+
+  console.log(multiple(5,6))
